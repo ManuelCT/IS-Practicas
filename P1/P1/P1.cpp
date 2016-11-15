90d463172954b83f0bf72b3f925cbceaa2cb0132
Manuel Castilla Toribio <manuel.castilla@live.u-tad.com>
2016-11-15 21:45:44 +0100

    Sesion 15/11/2016

--- P1/P1/P1.cpp
+++ P1/P1/P1.cpp
@@ -1,30 +1,29 @@
-// P1.cpp : Defines the entry point for the console application.
-//
-
-//_kbhit()
-//_getch()
-
 #include "stdafx.h"
 
-#define WORLD_WIDTH  52
+#define WORLD_WIDTH  51
 #define PLAYER_INITIAL_POSITION 25
 #define WORLD_SYMBOL '_'
 #define PLAYER_SYMBOL  '*'
 #define BULLET_SYMBOL '-'
+#define KEY_LEFT_MOVE 'a'
+#define KEY_RIGHT_MOVE 'd'
+#define KEY_LEFT_SHOT 'q'
+#define KEY_RIGHT_SHOT 'e'
 
 int main () {
-	//VARS DECLARATION
-	char cWorld[WORLD_WIDTH];
+	//VARIABLES DECLARATION
+	char cWorld[WORLD_WIDTH + 1];
 	char cKey;
 	unsigned int iPlayerPos = PLAYER_INITIAL_POSITION;
 	unsigned int iBulletPos;
-	unsigned int iBulletDirection = 0;
+	unsigned int iBulletDirection = 0; //0 -> No Bullet. 1 -> Left Direction Bullet. 2 -> Right Direction Bullet.
 
 	//WORLD INITIALITATION
 	for	(unsigned int i = 0; i < WORLD_WIDTH; i++) {
 		cWorld[i] = WORLD_SYMBOL;
 	}
-	cWorld[WORLD_WIDTH - 1] = '\0';
+
+	cWorld[WORLD_WIDTH] = '\0';
 	cWorld[iPlayerPos] = PLAYER_SYMBOL;
 	printf("\n\n\n\n%s", cWorld);
 
@@ -32,33 +31,29 @@ int main () {
 	while (true) {
 		if (_kbhit()) {
 			cKey = _getch();
-
-			if (cKey == 'a') {
+			if (cKey == KEY_LEFT_MOVE) {
 				if (iPlayerPos != 0) {
 					--iPlayerPos;
 					cWorld[iPlayerPos] = PLAYER_SYMBOL;
 					cWorld[iPlayerPos + 1] = WORLD_SYMBOL;
 				}
 			}
-
-			else if (cKey == 'd') {
-				if (iPlayerPos != (WORLD_WIDTH - 2)) {
+			else if (cKey == KEY_RIGHT_MOVE) {
+				if (iPlayerPos != WORLD_WIDTH) {
 					++iPlayerPos;
 					cWorld[iPlayerPos] = PLAYER_SYMBOL;
 					cWorld[iPlayerPos - 1] = WORLD_SYMBOL;
 				}
 			}
-
 			else if (iBulletDirection == 0) {
-				if (cKey == 'q') {
+				if (cKey == KEY_LEFT_SHOT) {
 					if (iPlayerPos != 0) {
 						iBulletPos = iPlayerPos - 1;
 						iBulletDirection = 1;
 					}
 				}
-
-				else if (cKey == 'e') {
-					if (iPlayerPos != (WORLD_WIDTH - 2)) {
+				else if (cKey == KEY_RIGHT_SHOT) {
+					if (iPlayerPos != WORLD_WIDTH) {
 						iBulletPos = iPlayerPos + 1;
 						iBulletDirection = 2;
 					}
@@ -71,14 +66,13 @@ int main () {
 			cWorld[iBulletPos] = BULLET_SYMBOL;
 			cWorld[iBulletPos + 1] = WORLD_SYMBOL;
 		}
-
 		else if (iBulletDirection == 2) {
 			++iBulletPos;
 			cWorld[iBulletPos] = BULLET_SYMBOL;
 			cWorld[iBulletPos - 1] = WORLD_SYMBOL;
 		}
 
-		if (iBulletPos == 0 || iBulletPos == (WORLD_WIDTH - 2)) {
+		if (iBulletPos == 0 || iBulletPos == WORLD_WIDTH) {
 			cWorld[iBulletPos] = WORLD_SYMBOL;
 			iBulletDirection = 0;
 			iBulletPos = WORLD_WIDTH / 2;
@@ -86,6 +80,7 @@ int main () {
 
 		system("cls");
 		printf("\n\n\n\n%s", cWorld);
+
 		Sleep(200);
 	}
 
---
+++ P2/P2/P2.cpp
@@ -0,0 +1,156 @@
+#include "stdafx.h"
+
+#define WORLD_WIDTH  30
+#define PLAYER_INITIAL_POSITION WORLD_WIDTH/2
+#define WORLD_SYMBOL '_'
+#define PLAYER_SYMBOL  '*'
+#define BULLET_SYMBOL '-'
+#define ENEMY_SYMBOL 'X'
+#define KEY_LEFT_MOVE 'a'
+#define KEY_RIGHT_MOVE 'd'
+#define KEY_LEFT_SHOT 'q'
+#define KEY_RIGHT_SHOT 'e'
+
+void resetBullet(int & iBulletPos, unsigned int & iBulletDirection, char * cWorld) {
+	cWorld[iBulletPos] = WORLD_SYMBOL;
+	iBulletDirection = 0;
+	iBulletPos = -1;
+}
+
+void resetEnemy(unsigned int & iEnemyPos, unsigned int & iEnemyDirection, char * cWorld) {
+	cWorld[iEnemyPos] = WORLD_SYMBOL;
+	if (rand() % 2 == 1) {
+		iEnemyPos = 0;
+		iEnemyDirection = 2;
+	}
+	else {
+		iEnemyPos = WORLD_WIDTH - 1;
+		iEnemyDirection = 1;
+	}
+}
+
+void setBulletPosition(int & iBulletPos, unsigned int & iBulletDirection, char * cWorld) {
+	if (iBulletDirection == 1) {
+		--iBulletPos;
+		cWorld[iBulletPos] = BULLET_SYMBOL;
+		cWorld[iBulletPos + 1] = WORLD_SYMBOL;
+	}
+	else if (iBulletDirection == 2) {
+		++iBulletPos;
+		cWorld[iBulletPos] = BULLET_SYMBOL;
+		cWorld[iBulletPos - 1] = WORLD_SYMBOL;
+	}
+}
+
+//Sets enemy positions or resets its position
+void setEnemyPosition(unsigned int & iEnemyPos, unsigned int & iEnemyDirection, int & iBulletPos, unsigned int & iBulletDirection, char * cWorld) {
+	if (abs(static_cast<int>(iEnemyPos-iBulletPos)) <= 1) {
+		resetBullet(iBulletPos, iBulletDirection, cWorld);
+		resetEnemy(iEnemyPos, iEnemyDirection, cWorld);
+	}
+	else if (iEnemyDirection == 1) {
+		if (iEnemyPos != 0) {
+			--iEnemyPos;
+			cWorld[iEnemyPos] = ENEMY_SYMBOL;
+			cWorld[iEnemyPos + 1] = WORLD_SYMBOL;
+		}
+		else
+			resetEnemy(iEnemyPos, iEnemyDirection, cWorld);
+	}
+	else {
+		if (iEnemyPos != (WORLD_WIDTH - 1)) {
+			++iEnemyPos;
+			cWorld[iEnemyPos] = ENEMY_SYMBOL;
+			cWorld[iEnemyPos - 1] = WORLD_SYMBOL;
+		}
+		else
+			resetEnemy(iEnemyPos, iEnemyDirection, cWorld);
+	}
+}
+
+//Checks if the player is still alive
+bool checkPlayer(unsigned int & iPlayerPos, unsigned int & iEnemyPos) {
+	if (abs(static_cast<int>(iEnemyPos - iPlayerPos)) <= 1) {
+		return false;
+	}
+	return true;
+}
+
+int main() {
+	//VARIABLES DECLARATION
+	char cWorld[WORLD_WIDTH + 1];
+	char cKey;
+	unsigned int iPlayerPos = PLAYER_INITIAL_POSITION;
+	int iBulletPos;
+	unsigned int iEnemyPos = 0;
+	unsigned int iBulletDirection = 0; //0 -> No Bullet. 1 -> Left Direction Bullet. 2 -> Right Direction Bullet.
+	unsigned int iEnemyDirection = 0; //0 -> No Enemy. 1 -> Left Direction Enemy. 2 -> Right Direction Enemy.
+	srand(static_cast<unsigned int>(time(NULL)));
+
+	//WORLD INITIALITATION
+	for (unsigned int i = 0; i < WORLD_WIDTH; ++i) {
+		printf("%d\n", i);
+		cWorld[i] = WORLD_SYMBOL;
+	}
+
+	resetEnemy(iEnemyPos, iEnemyDirection, cWorld);
+
+	cWorld[WORLD_WIDTH] = '\0';
+	cWorld[iPlayerPos] = PLAYER_SYMBOL;
+	printf("\n\n\n\n%s", cWorld);
+
+	//GAME LOOP
+	while (checkPlayer(iPlayerPos, iEnemyPos)) {
+		if (_kbhit()) {
+			cKey = _getch();
+			if (cKey == KEY_LEFT_MOVE) {
+				if (iPlayerPos != 0) {
+					--iPlayerPos;
+					cWorld[iPlayerPos] = PLAYER_SYMBOL;
+					cWorld[iPlayerPos + 1] = WORLD_SYMBOL;
+				}
+			}
+			else if (cKey == KEY_RIGHT_MOVE) {
+				if (iPlayerPos != WORLD_WIDTH) {
+					++iPlayerPos;
+					cWorld[iPlayerPos] = PLAYER_SYMBOL;
+					cWorld[iPlayerPos - 1] = WORLD_SYMBOL;
+				}
+			}
+			else if (iBulletDirection == 0) {
+				if (cKey == KEY_LEFT_SHOT) {
+					if (iPlayerPos != 0) {
+						iBulletPos = iPlayerPos - 1;
+						iBulletDirection = 1;
+					}
+				}
+				else if (cKey == KEY_RIGHT_SHOT) {
+					if (iPlayerPos != (WORLD_WIDTH - 1)) {
+						iBulletPos = iPlayerPos + 1;
+						iBulletDirection = 2;
+					}
+				}
+			}
+		}
+
+		setBulletPosition(iBulletPos, iBulletDirection, cWorld);
+
+		if (iEnemyDirection != 0) {
+			setEnemyPosition(iEnemyPos, iEnemyDirection, iBulletPos, iBulletDirection, cWorld);
+		}
+
+		if (iBulletPos == 0 || iBulletPos == (WORLD_WIDTH - 1)) {
+			resetBullet(iBulletPos, iBulletDirection, cWorld);
+		}
+
+		system("cls");
+		printf("\n\n\n\n%s", cWorld);
+
+		Sleep(200);
+	}
+	system("cls");
+	printf("GAME OVER");
+	getchar();
+	return 0;
+}
+
